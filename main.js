210550b27a75320d7dc338233bc57bc66dd3a58a
Akupowar2403 <powarakanksha9188@gmail.com>
2026-05-31 15:10:50 +0530

    feat: add app icon (bg removed), use .ico for Windows

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createWindow() {
     minWidth: 1024,
     minHeight: 600,
     title: 'Fenyxn Lab Management',
+    icon: path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
